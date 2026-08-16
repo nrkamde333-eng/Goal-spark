d16dd2d861295f10636a7ee4dd117a6e4c63e6c5
nrkamde333-eng <296819752+nrkamde333-eng@users.noreply.github.com>
2026-08-16 14:12:21 +0530

    feat: add loading state and optimize utility library
    Implemented a loading spinner in index.html to improve perceived performance. Replaced external clsx and tailwind-merge dependencies with a lightweight custom implementation in utils.ts to reduce bundle size.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,57 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+type ClassValue = ClassValue[] | string | number | boolean | null | undefined | { [key: string]: any };
+
+function toVal(mix: ClassValue): string {
+  let k: string;
+  let y: string;
+  let str = "";
+
+  if (!mix) return "";
+
+  if (typeof mix === "string" || typeof mix === "number") {
+    str += mix;
+  } else if (typeof mix === "object") {
+    if (Array.isArray(mix)) {
+      const len = mix.length;
+      for (let i = 0; i < len; i++) {
+        if (mix[i]) {
+          if ((y = toVal(mix[i]))) {
+            if (str) str += " ";
+            str += y;
+          }
+        }
+      }
+    } else {
+      for (k in mix) {
+        if (mix[k]) {
+          if (str) str += " ";
+          str += k;
+        }
+      }
+    }
+  }
+
+  return str;
+}
+
+export function clsx(...inputs: ClassValue[]): string {
+  let i = 0;
+  let tmp: ClassValue;
+  let x: string;
+  let str = "";
+  const len = inputs.length;
+  for (; i < len; i++) {
+    if ((tmp = inputs[i])) {
+      if ((x = toVal(tmp))) {
+        if (str) str += " ";
+        str += x;
+      }
+    }
+  }
+  return str;
+}
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return clsx(...inputs);
 }
+
+
